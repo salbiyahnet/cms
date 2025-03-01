0afe98185d0c47df99f0f125904b30ea40a9f3bd
Bery Indo <bery.indo@gmail.com>
2025-03-02 01:43:34 +0700

    Update install_docker.sh

--- install_docker.sh
+++ install_docker.sh
@@ -23,7 +23,7 @@ echo \
   
 sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 sudo docker run hello-world
 
